a2182147fc2cee6167b4bcb4df2c916751bda7ab
Ilya Tyunin <tomslavg96@gmail.com>
2023-06-28 14:27:01 +0300

    delete attach

--- src/test/java/ru/ffin/tests/TestBase.java
+++ src/test/java/ru/ffin/tests/TestBase.java
@@ -16,17 +16,13 @@ public class TestBase {
     static void firstConfigure() {
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
+
     }
     @BeforeEach
     void addListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
+
     @AfterEach
     void addAttachments() {
         Attach.screenshotAs("Last Screeshot");
