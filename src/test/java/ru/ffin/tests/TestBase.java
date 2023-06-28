7aeb349a3462e88b99971a08f7edb99193f29994
Ilya Tyunin <tomslavg96@gmail.com>
2023-06-28 14:28:08 +0300

    delete attach

--- src/test/java/ru/ffin/tests/TestBase.java
+++ src/test/java/ru/ffin/tests/TestBase.java
@@ -15,20 +15,9 @@ public class TestBase {
     @BeforeAll
     static void firstConfigure() {
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
-
     }
     @BeforeEach
     void addListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-    }
-
-    @AfterEach
-    void addAttachments() {
-        Attach.screenshotAs("Last Screeshot");
-        Attach.pageSource();
-        Attach.browserConsoleLogs();
-        Attach.addVideo();
-        Attach.getVideoUrl();
     }
 }
